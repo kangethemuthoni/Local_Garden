202dd8ff96712a2acd1d21adfd50b5a92377ee61
Muthoni Kang'ethe <muthoniikangethe@gmail.com>
2021-08-11 15:55:36 +0300

    Add Cart Sidebar

---
+++ cart.js
@@ -0,0 +1,9 @@
+function openCartMenu () {
+    document.getElementById("mySidebar").style.width = "310px";
+    document.getElementById("btncart").style.marginRight = "310px";
+}
+
+function closeCartMenu () {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("btncart").style.marginRight= "0";
+}
