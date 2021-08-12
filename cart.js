a337ecb9e3c7ceba5ff1d273b75745f1528dd1cc
Muthoni Kang'ethe <muthoniikangethe@gmail.com>
2021-08-12 16:07:12 +0300

    1. Add 'Remove Cart Items' function 2. Add 'Cart Quantity Check' function

--- cart.js
+++ cart.js
@@ -8,12 +8,52 @@ function closeCartMenu () {
     document.getElementById("btncart").style.marginRight= "0";
 }
 
-/* 
+// start - Remove Cart Items Section
 
-- Price (Get total price) - price * quantity
-- Update cart total
-- Add items to cart - Add Cart Rows - Item, Price, Quantity
-- Remove items from cart 
-- Go to checkout button (OnClick button to Checkout page)
+// Get all buttons
+var removeCartItemButtons = document.getElementsByClassName("btn-danger");
 
-*/
+// Iterate all buttons and add an event listener
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+
+    var removeCartItemButton = removeCartItemButtons[i];
+    removeCartItemButton.addEventListener("click", removeCartItem);
+}
+
+// The function to remove cart item
+function removeCartItem (event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+}
+
+// end - Remove Cart Items Section
+
+
+// start - Cart Quantity Check (Should not be less than one)
+
+var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+
+for (var i = 0; i < quantityInputs.length; i++) {
+
+    var quantityInput = quantityInputs[i];
+    quantityInput.addEventListener("change", quantityChanged);
+}
+
+function quantityChanged (event) {
+    var input = event.target;
+
+    // If the change event changes the value to one that's not a number or one that is less than or equal 
+    // to zero, then the value is kept constant at 1
+    if (isNaN (input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+}
+
+// end - Cart Quantity Check (Should not be less than one)
+
+
+// start - Update Cart Total Section
+
+
+
+// end - Update Cart Total Section
