71a95801af7d43fb3ce4859d0960feeb2e4068a9
Muthoni Kang'ethe <muthoniikangethe@gmail.com>
2021-08-13 16:18:14 +0300

    1. Add 'Update Cart Total' function 2. Add 'Add to Cart' function

--- cart.js
+++ cart.js
@@ -1,12 +1,12 @@
 function openCartMenu () {
     document.getElementById("mySidebar").style.width = "310px";
-    document.getElementById("btncart").style.marginRight = "310px";
 }
 
 function closeCartMenu () {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("btncart").style.marginRight= "0";
 }
+
+var count = 0;
 
 // start - Remove Cart Items Section
 
@@ -24,12 +24,18 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
 function removeCartItem (event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.remove();
+
+    updateCartTotal ();
+
+    // Update Cart Icon
+    count--;
+    document.getElementById("cartspan").innerText = count;
 }
 
 // end - Remove Cart Items Section
 
 
-// start - Cart Quantity Check (Should not be less than one)
+// start - Quantity Changed Function
 
 var quantityInputs = document.getElementsByClassName("cart-quantity-input");
 
@@ -42,18 +48,127 @@ for (var i = 0; i < quantityInputs.length; i++) {
 function quantityChanged (event) {
     var input = event.target;
 
+    // Cart Quantity Check (Should not be less than one)
     // If the change event changes the value to one that's not a number or one that is less than or equal 
     // to zero, then the value is kept constant at 1
     if (isNaN (input.value) || input.value <= 0) {
         input.value = 1;
     }
+
+    updateCartTotal ();
+
+    // Update Cart Icon 
+    count++;
+    document.getElementById("cartspan").innerText = count;
+
 }
 
-// end - Cart Quantity Check (Should not be less than one)
+// end - Quantity Changed Function
 
 
 // start - Update Cart Total Section
 
+function updateCartTotal () {
 
+    var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+    var total = 0;
+
+    for (var i = 0; i < cartRows.length; i++) {
+
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+        var price = parseFloat(priceElement.innerText.replace("Kes ", ""));
+        var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+
+    }
+
+    total = Math.round(total * 100) / 100; // Two decimal points
+    document.getElementsByClassName("cart-total-price")[0].innerText = "Kes " + total;
+
+}
 
 // end - Update Cart Total Section
+
+
+// start - Add to Cart Section
+
+var addItemToCartButtons = document.getElementsByClassName("shop-item-button");
+
+for (var i = 0; i < addItemToCartButtons.length; i++) {
+
+    var addItemToCartButton = addItemToCartButtons[i];
+    addItemToCartButton.addEventListener("click", addItemToCartClicked);
+
+}
+
+// Getting the title and price to be used in the addItemToCart function
+// Use the button that triggers the event, and get the parent elements in order to get title and price
+function addItemToCartClicked (event) {
+    var button = event.target;
+    var shopItem = button.parentElement;
+
+    var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+    var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+
+    console.log(title, price);
+
+    addItemToCart(title, price);
+    updateCartTotal();
+
+}
+
+function addItemToCart (title, price) {
+
+    // Create a div named cartRow
+    var cartRow = document.createElement("div");
+    // Add the cart-row class to the div created
+    cartRow.classList.add("cart-row");
+
+    // Get item titles 
+    var cartItems = document.getElementsByClassName("cart-items")[0];
+    var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+
+    // Check by title whether Cart Items have already been added to cart 
+    for (var i = 0; i < cartItemNames.length; i++) {
+
+        cartItemName = cartItemNames[i];
+
+        if (cartItemName.innerText == title) {
+
+            alert("This Item is already added to the Cart!");
+            return;
+
+        }
+    }
+
+    // Adding created div cart-row to cart
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <span class="cart-item-title">${title}</span>
+        </div>
+
+        <span class="cart-price cart-column">${price}</span>
+
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">x</button>
+        </div>
+    `
+
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+
+    // Add event listeners to the new created div cart-row
+    cartRow.getElementsByClassName("btn-danger")[0].addEventListener("click", removeCartItem);
+    cartRow.getElementsByClassName("cart-quantity-input")[0].addEventListener("change", quantityChanged);
+
+    // Update Cart Icon 
+    count++;
+    document.getElementById("cartspan").innerText = count;
+
+}
+
+// end - Add to Cart Section
